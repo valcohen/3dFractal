1cb6056fb0d9570bf9053046eb9d5ceac7f8fa3f
Val Cohen <val@valcohen.com>
2018-05-19 10:21:57 -0700

    Add camera movement script

---
+++ Assets/Fractal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour {
+
+    public Mesh mesh;
+    public Material material;
+
+	void Start () {
+        gameObject.AddComponent<MeshFilter> ().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer> ().material = material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
